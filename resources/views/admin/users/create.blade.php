4a8a891201fccc0a79b3534b3c0f03715c423733
ManuelMahlerHutter <manuelmahlerhutter@gmail.com>
2017-12-21 14:09:36 +0100

    users able to presist

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,1 +1,51 @@
-create.blade.php
+//@extends('layouts.admin')
+
+@section('content')
+<h1>Create Users</h1>
+
+
+
+
+{!! Form::open(['method' => 'POST' , 'action'=>'AdminUsersController@store', 'files'=>true]) !!}
+{{ csrf_field() }}
+<div class="form-group">
+	{!! Form::label('name', 'Name') !!}
+	{!! Form::text('name', null, ['class'=>'form-control']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::label('email', 'Email') !!}
+	{!! Form::email('email', null, ['class'=>'form-control']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::label('role_id', 'Role') !!}
+	{!! Form::select('role_id',[''=>'choose options'] + $roles, 1, ['class'=>'form-control']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::label('password', 'Password') !!}
+	{!! Form::password('password',  ['class'=>'form-control']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::label('is_active', 'is_active') !!}
+	{!! Form::select('is_active', array(1=>'Active', 0=>'not Active'),0, ['class'=>'form-control']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::label('File', 'Title') !!}
+	{!! Form::file('file', null, ['class'=>'form-control']) !!}
+</div>
+
+
+<div class="form-group">
+	{!! Form::submit('Create User', ['class'=> 'btn btn-primary']) !!}
+</div>
+{!!Form::close() !!}	
+
+@include('includes.errors')
+
+
+@endsection
+
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,44 @@
-<h1>index.blade.php</h1>
+@extends('layouts.admin')
+
+
+@section('content')
+<h1>Users</h1>
+
+<table class="table table-striped">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>email</th>
+        <th>active</th>
+        <th>role</th>
+        <th>created</th>
+        <th>updated</th>
+        <
+      </tr>
+    </thead>
+    <tbody>
+
+    	@if($users)
+
+    	@foreach($users as $user)
+
+      <tr>
+        <td>{{$user->id}}</td>
+       <td>{{$user->name}}</td>
+       <td>{{$user->email}}</td>
+       <td>{{$user->is_active == 1 ? 'Active' : 'Not active'}}</td>
+       <td>{{$user->role->name}}</td>
+       <td>{{$user->created_at->diffForHumans()}}</td>
+       <td>{{$user->updated_at->diffForHumans()}}</td>
+      </tr>
+
+      @endforeach
+
+      @endif
+ 
+     
+    </tbody>
+  </table>
+
+@endsection
